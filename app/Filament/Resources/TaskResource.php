0266615dafcdba707c2aa94536d25eacc958535d
salimanmohamed20 <salimanmohamed20@gmail.com>
2026-01-04 15:36:42 +0200

    up

--- app/Filament/Resources/TaskResource.php
+++ app/Filament/Resources/TaskResource.php
@@ -51,7 +51,18 @@ class TaskResource extends Resource
     {
         return parent::getGlobalSearchEloquentQuery()->with(['project']);
     }
-    
+    use Filament\Navigation\NavigationItem;
+
+public static function getNavigationBadge(): ?string
+{
+    return (string) static::getModel()::count();
+}
+
+public static function getNavigationBadgeColor(): ?string
+{
+    return 'primary'; // success | warning | danger
+}
+
     public static function getGlobalSearchResultUrl($record): string
     {
         return TaskResource::getUrl('edit', ['record' => $record]);
