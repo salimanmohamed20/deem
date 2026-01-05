5ec5bb011a6c73a7da6cb56064ba9e2491f895f2
salimanmohamed20 <salimanmohamed20@gmail.com>
2026-01-05 10:27:48 +0200

    up

--- app/Filament/Resources/ProjectResource/Pages/ListProjects.php
+++ app/Filament/Resources/ProjectResource/Pages/ListProjects.php
@@ -6,7 +6,7 @@ use App\Filament\Resources\ProjectResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
-use Filament\Resources\Pages\ListRecords\Tab;
+use Filament\Tables\Tabs\Tab;
 use App\Models\Project;
 
 
